1d00d49eb1dab388620ebfd15d91eb865c34b5e1
haoc <haoc@rihuisoft.com>
2020-03-13 16:09:58 +0800

    test-config-client

---
+++ test-config-client/src/main/java/gyaku/bearschool/testconfigclient/TestConfigClientApplication.java
@@ -0,0 +1,13 @@
+package gyaku.bearschool.testconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestConfigClientApplication.class, args);
+    }
+
+}
---
+++ test-config-client/src/test/java/gyaku/bearschool/testconfigclient/TestConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package gyaku.bearschool.testconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
