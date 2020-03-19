9289372746f5100f626993a305a4ba3764c53e66
haoc <haoc@rihuisoft.com>
2020-03-19 11:08:57 +0800

    权鉴初始化

--- admin-server/src/main/java/gyaku/bearschool/admin/adminserver/controller/AdminController.java
+++ admin-server/src/main/java/gyaku/bearschool/admin/adminserver/controller/AdminController.java
@@ -4,8 +4,10 @@ import gyaku.bearschool.admin.adminserver.service.UserService;
 import gyaku.bearschool.common.servercommon.domain.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,11 +18,16 @@ public class AdminController {
     @Autowired
     UserService userService;
 
+    @RequestMapping(value = "/current", method = RequestMethod.GET)
+    public Principal getUser(Principal principal) {
+        return principal;
+    }
+
     /**
      * 获取所有用户信息
      * @return
      */
-    @RequestMapping(value = "/getAllUser")
+    @RequestMapping(value = "/getAllUser", method = RequestMethod.POST)
     public JsonResult getAllUser(){
         Map<String, Object> map = new HashMap<>();
         map.put("userList", userService.findAll());
