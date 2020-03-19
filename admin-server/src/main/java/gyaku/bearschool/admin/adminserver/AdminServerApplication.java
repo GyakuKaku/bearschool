efc2abf722cb42ac26d18150273d8c90e7d5ed2c
haoc <haoc@rihuisoft.com>
2020-03-19 13:59:23 +0800

    权鉴系统

--- admin-server/src/main/java/gyaku/bearschool/admin/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/gyaku/bearschool/admin/adminserver/AdminServerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
