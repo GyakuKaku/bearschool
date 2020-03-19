c9f845808c209bde87c706728b2c188e9a91e8a3
haoc <haoc@rihuisoft.com>
2020-03-19 12:49:46 +0800

    mybatis变为注解形式

--- server-common/src/main/java/gyaku/bearschool/common/servercommon/dao/UserDao.java
+++ server-common/src/main/java/gyaku/bearschool/common/servercommon/dao/UserDao.java
@@ -1,15 +1,13 @@
 package gyaku.bearschool.common.servercommon.dao;
 
 import gyaku.bearschool.common.servercommon.domain.User;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface UserDao {
-
-    User find(Map<String, Object> param);
-
+    @Select("SELECT * FROM user")
     List<User> findAll();
 }
